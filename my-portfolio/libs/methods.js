3a9fb945751c570e5e164945a61f589a1ca99857
Spencer <s.clarkegriffiths@gmail.com>
2024-02-23 10:09:39 +0000

    now retrieving projects and altering the id from a mongo object to remove warnings

--- my-portfolio/libs/methods.js
+++ my-portfolio/libs/methods.js
@@ -9,17 +9,17 @@ export const getProjects = async () => {
         await connectMongoDB(); // Ensure connection is established
         const projects = await Project.find();
         const modifiedProjects = projects.map((project) => {
-        let modifiedProject = { ...project, _id: project._id.toString()};
-        // If you're working directly with MongoDB objects, you might need to convert the project to a plain object first
-        // let modifiedProject = { ...project.toObject(), _id: project._id.toString() };
-        return modifiedProject;}) 
+            let newPro = {...project.toObject(), _id: project._id.toString()};
+            return newPro;
+        });
+        return modifiedProjects
     } catch(error) { 
         console.error("Error in Methods- Get Projects:", error);
     }
 };
 
 export const getJobs = async () => { 
-    try { 
+    try {
         const jobs = await Work.find()
         return jobs
     } catch (error) { 
