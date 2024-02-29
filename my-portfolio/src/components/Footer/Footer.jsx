e0e15b14899bf260145737434bbafb575382388f
Spencer <s.clarkegriffiths@gmail.com>
2024-02-29 21:28:28 +0000

    commit for hosting

--- my-portfolio/src/components/Footer/Footer.jsx
+++ my-portfolio/src/components/Footer/Footer.jsx
@@ -1,9 +1,9 @@
-// const Footer = () => { 
+const Footer = () => { 
 //     // return ( 
 //     //     <>
 //     //     <span> Footer </span>
 //     //     </>
 //     // )
-// }
+}
 
-// export default Footer
+export default Footer
