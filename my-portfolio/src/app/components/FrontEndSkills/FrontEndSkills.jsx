a1ec4b0c8b069a9ed19c6234bfddbf8d661aff76
Spencer <s.clarkegriffiths@gmail.com>
2024-03-07 22:16:03 +0000

    removed bullet points

--- my-portfolio/src/app/components/FrontEndSkills/FrontEndSkills.jsx
+++ my-portfolio/src/app/components/FrontEndSkills/FrontEndSkills.jsx
@@ -19,15 +19,11 @@ const FrontEndSkills = () => {
         <p className={styles.titleProgramming} data-aos="fade-left">HTML, CSS and React</p>
         <p className={styles.titleProgramming} data-aos="fade-left">Axios for managing HTTP requests</p>
         <p className={styles.titleProgramming} data-aos="fade-left">Hosting with Cloudflare, Netlify and Vercel </p>
-        <ul>
+        <ul className={styles.unList}>
             <li><p className={styles.contentProgramming} data-aos="fade-left">Proficient in optimising web performance with Lighthouse</p></li>
             <li><p className={styles.contentProgramming} data-aos="fade-left">Using WAVE Web (Accessibility Evaluation Tools) and other accessibility tools</p></li>
             <li><p className={styles.contentProgramming} data-aos="fade-left">Understanding and applying Web Content Accessibility Guidelines (WCAG)</p></li>
         </ul>
-        <ul>
-            <li><p className={styles.contentProgramming} data-aos="fade-left"></p></li>
-        </ul>
-
         </section>
         </article>
     )
